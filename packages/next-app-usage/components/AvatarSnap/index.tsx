66f72ab71eec5657250422e19c2f3c8beac8c0eb
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-24 20:59:02 -0400

    added infura

--- packages/next-app-usage/components/AvatarSnap/index.tsx
+++ packages/next-app-usage/components/AvatarSnap/index.tsx
@@ -2,6 +2,11 @@ import * as React from 'react'
 import { isSnapInstalled } from './utils';
 import { Loading } from '@web3uikit/core';
 
+import {Edit, CrossCircle} from '@web3uikit/icons'
+import { create, CID, IPFSHTTPClient } from "ipfs-http-client";
+import {useEffect} from "react";
+
+
 
 declare global {
     interface Window {
@@ -15,7 +20,7 @@ declare global {
 
 export const snapId = 'local:http://localhost:8086'
 
-const Index:React.FC = () => {
+const AvatarSnap:React.FC = () => {
 
     const [uploading, setUploading] = React.useState(false)
     const [snapState, setSnapState] = React.useState<{
@@ -72,6 +77,11 @@ const Index:React.FC = () => {
         }
     }, [snapState.isInstalled])
 
+
+    if(uploading){
+        return <Uploader onClose={()=>setUploading(false)}/>
+    }
+
     if (!snapState.isInstalled){
         return <div>
             Not installed. Click here to install
@@ -85,11 +95,68 @@ const Index:React.FC = () => {
     else if (snapState.isInstalled && !snapState.avatar){
         return <Loading />
     } else {
-        return   ( <div>
-            <img src={snapState.avatar}/>
-        </div>)
+        return   (
+            <div>
+            <img className="object-scale-down h-48" src={snapState.avatar}/>
+                <div className='flex'>
+                    <Edit fontSize='20px' onClick={()=>setUploading(true)}/>
+                </div>
+
+        </div>
+        )
     }
 
 }
 
-export default Index
+
+export default AvatarSnap
+
+const Uploader: React.FC<{onClose: ()=>void}> = ({onClose}) => {
+    console.log(process.env.NEXT_PUBLIC_INFURA_ID)
+    const [ipfs, setIpfs] = React.useState<IPFSHTTPClient | undefined>()
+    useEffect(()=>{
+        try {
+            const ipfsConn = create({
+                url: "https://ipfs.infura.io:5001/api/v0",
+            });
+            setIpfs(ipfsConn)
+
+        } catch (error) {
+            console.error("Could not connect to IPFS", error);
+        }
+    },[])
+
+
+    const handleIPFSSubmit = async (event: React.FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+
+        const form = event.target as HTMLFormElement;
+        const files = (form[0] as HTMLInputElement).files;
+
+        if (!files || files.length === 0) {
+            return alert("No files selected");
+        }
+
+        const file = files[0];
+        // upload files
+        const result = await (ipfs as IPFSHTTPClient).add(file);
+
+        console.log('event',event, 'result', result)
+    }
+
+    return <div>
+        <p>Upload File using IPFS</p>
+        {
+            (ipfs) ?  <form onSubmit={handleIPFSSubmit}>
+                <input name="file" type="file" />
+
+                <button type="submit">Upload File</button>
+            </form>:<div>
+            Could not connect to IPFS at this time
+            </div>
+        }
+
+
+        <CrossCircle onClick={onClose}/>
+    </div>
+}
