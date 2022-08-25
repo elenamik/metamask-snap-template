ab2ba519bc4fddc8837f4bb2b0f8a6c5bd5ae2e3
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-25 16:59:45 -0400

    fixing some things

--- packages/avatar-snap/snap.config.js
+++ packages/avatar-snap/snap.config.js
@@ -2,5 +2,6 @@ module.exports = {
   cliOptions: {
     src: './src/index.ts',
     port: 8086,
+    outfileName: "bundle.js"
   },
 };
--- packages/avatar-snap/src/interfaces.ts
+++ packages/avatar-snap/src/interfaces.ts
@@ -1,12 +1,2 @@
 import {JsonRpcId, JsonRpcVersion} from "@metamask/types";
 
-export interface SnapState {
-    image?: string,
-}
-
-export interface JsonRpcRequest<T, M> {
-    jsonrpc: JsonRpcVersion;
-    method: M,
-    id: JsonRpcId;
-    params?: T;
-}
